d85ed37aab4645fff31eca0323748c49a3e13af9
Shreyas Daniel <iamshreydan@gmail.com>
2021-06-25 12:25:52 +0530

    created landing component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { useState } from 'react'
 import './App.css'
-
+import Landing from './Components/Landing-Desktop/Landing'
 
 function App() {
   return (
     <div className="App">
-      
+      <Landing />
     </div>
   );
 }
--- src/Components/Landing-Desktop/Landing.jsx
+++ src/Components/Landing-Desktop/Landing.jsx
@@ -1,6 +1,7 @@
-import React, {useState} from 'react'
+import React from 'react'
 import './Landing.css'
 import logo from '../../assets/kw.png'
+import landingimage from '../../assets/kawatch--watchface.png'
 
 function Landing() {
 
@@ -8,7 +9,7 @@ function Landing() {
     return (
         <div id="landing">
             <div className="nav">
-                <img src={logo} alt="" />
+                <img src={logo} alt="" id="logo"/>
 
                 <div className="menu-bar">
                     <div>Home</div>
@@ -18,7 +19,15 @@ function Landing() {
 
             </div>
 
-            <div className="content"></div>
+            <div className="content">
+                <div id="text">
+                    <div id="title">Kawatch <br/> S1</div>
+                    <div id="tagline">A watch that assists you to be your best everywhere.</div>
+
+                    <div id="button-3d">Experience in 3D</div>
+                </div>
+                <img src={landingimage} alt="" />
+            </div>
         </div>
     )
 }
